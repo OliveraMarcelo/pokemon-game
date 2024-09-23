a07a507d09844fbf0352b9d47ceaf716bf623ce8
Marcelo Olivera <marceloo@exo.com.ar>
2024-09-23 01:18:58 -0300

    FEAT(PokemonGame): crear estructura de la app, vistas y componentes

---
+++ src/modules/pokemons/components/PokemonPicture.vue
@@ -0,0 +1,20 @@
+<template>
+   <section >
+    <img class="brightness-0" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/132.svg" alt="" srcset="">
+   </section>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+img{
+    height: 200px;
+    user-select: none;
+    -moz-user-select: none;
+    -ms-user-select: none;
+    -webkit-user-drag: none;
+    -webkit-user-select: none;
+}
+</style>
---
+++ src/modules/pokemons/pages/PokemonGame.vue
@@ -0,0 +1,24 @@
+<template>
+    <section v-if="false" class="flex flex-col justify-center items-center w-screen h-screen">
+        <h1 class="text-3xl"> Espere por favor</h1>
+        <h3 class="animate-pulse">Cagando pokemons</h3>
+    </section>
+    <section class="flex flex-col justify-center items-center w-screen h-screen">
+        <h1 class="text-3xl m-2"> Quien es este pokemon ? </h1>
+        <!-- Pokemon picture -->
+        <PokemonPicture/>
+        <!-- Pokemon options -->
+        <PokemonOptions/>
+
+    </section>
+</template>
+
+<script setup>
+import PokemonPicture from "../components/PokemonPicture.vue"
+import PokemonOptions from "../components/PokemonOptions.vue"
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
