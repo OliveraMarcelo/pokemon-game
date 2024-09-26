1923c2f3da5abef8c8067faf8e2f77ad9b831477
Marcelo Olivera <marceloo@exo.com.ar>
2024-09-26 01:26:41 -0300

    FEAT(usePokemonGame) agregar funcion para obtener pokemones aleatorios, obtener 4 pokemones, y tener propiedades computadas para cargar la pagina

--- src/modules/pokemons/composables/usePokemonGame.ts
+++ src/modules/pokemons/composables/usePokemonGame.ts
@@ -1,18 +1,52 @@
-import { onMounted, ref } from "vue"
-import { GameStatus, type PokemonListResponse } from "../interfaces"
+import { computed, onMounted, ref } from "vue"
+import { type Pokemon, type PokemonListResponse, GameStatus} from "../interfaces"
 import { pokemonApi } from "../api/pokemonApi";
 
 export const usePokemonGame = ()=>{
 const gameStatus = ref<GameStatus>(GameStatus.Playing);
+const pokemons = ref<Pokemon[]>([])
+const pokemonsOptions = ref<Pokemon[]>([])
 
-const getPokemons =async ()=>{
+//propiedad computada cuando estoy cargando
+const isLoading = computed(()=>pokemons.value.length === 0);
+
+const getPokemons =async ():Promise<Pokemon[]> =>{
     const response = await pokemonApi.get<PokemonListResponse>('/?limit=151');
-    console.log(response.data.results)};
+    const pokemonsArray = response.data.results.map(pokemon =>{
+        const urlParts = pokemon.url.split('/');
+        const id = urlParts.at(-2) ?? 0
+        return {
+            name : pokemon.name,
+            id : +id
+        }
+    })
+    console.log(response.data.results)
+    return pokemonsArray.sort(()=> Math.random() - 0.5 );
+};
+const getNextOptions = ( howMany: number = 4)=>{
+    gameStatus.value = GameStatus.Playing
+    pokemonsOptions.value= pokemons.value.slice(0,howMany)
+    pokemons.value= pokemons.value.slice(howMany)
 
-    onMounted(()=>{
-        getPokemons();
+}
+//mezclemos de manera aleatorioa
+    onMounted(async()=>{
+        /* 
+        simulamos un tiempo de carga
+        await new Promise(resolve =>setTimeout(resolve,1000));
+         */
+        pokemons.value = await getPokemons();
+        getNextOptions()
+        console.log(pokemons.value)
+        console.log(pokemonsOptions.value)
+
     })
 return{
-    gameStatus
+    gameStatus,
+    isLoading,
+    pokemonsOptions,
+    //methods
+    pokemons,
+
 }
 }
--- src/modules/pokemons/pages/PokemonGame.vue
+++ src/modules/pokemons/pages/PokemonGame.vue
@@ -1,5 +1,5 @@
 <template>
-    <section v-if="false" class="flex flex-col justify-center items-center w-screen h-screen">
+    <section v-if="isLoading" class="flex flex-col justify-center items-center w-screen h-screen">
         <h1 class="text-3xl"> Espere por favor</h1>
         <h3 class="animate-pulse">Cagando pokemons</h3>
     </section>
@@ -17,7 +17,7 @@
 import PokemonPicture from "../components/PokemonPicture.vue"
 import PokemonOptions from "../components/PokemonOptions.vue"
 import  { usePokemonGame } from "../composables/usePokemonGame.ts"
-const { gameStatus } = usePokemonGame()
+const { gameStatus ,isLoading} = usePokemonGame()
 
 </script>
 
